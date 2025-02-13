16d6fc889fd3da8bd9b32e2c1699674342adea3a
AxelDanielMRS <l19120200@morelia.tecnm.mx>
2025-02-13 13:41:44 -0600

    segundo intento xd ESLinit y usar PixelSans

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,21 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Pixelify_Sans  } from "next/font/google";
+import { Geist, Geist_Mono, Pixelify_Sans } from "next/font/google";
 import './globals.css';
 
+// Configura la fuente Pixelify Sans
 const pixelifySans = Pixelify_Sans({
   subsets: ['latin'],
   weight: ['400', '700'], // Especifica los pesos que necesitas
-  variable: '--font-pixelify', // Opcional: Define una variable CSS
+  variable: '--font-pixelify', // Define una variable CSS
 });
 
-
+// Configura la fuente Geist Sans
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
+// Configura la fuente Geist Mono
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
@@ -30,10 +32,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" className={`${pixelifySans.variable} ${geistSans.variable} ${geistMono.variable}`}>
+      <body className="antialiased">
         {children}
       </body>
     </html>
